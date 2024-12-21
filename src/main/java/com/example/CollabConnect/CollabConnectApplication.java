ae2020c426d387f6f5b02d3695eb227cabc28652
M-coder-design <ghoshmayukh152@gmail.com>
2024-12-21 10:12:54 +0530

    changes

---
+++ src/main/java/com/example/CollabConnect/CollabConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CollabConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CollabConnect/CollabConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CollabConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
